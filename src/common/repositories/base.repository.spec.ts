7d31e0134f8c718007da524fd8105ab60ee7a5a4
Rangga Adhitya Prawira <deneuv3.4@gmail.com>
2018-07-29 18:25:34 +0700

    add test to base repository

---
+++ src/common/repositories/base.repository.spec.ts
@@ -0,0 +1,114 @@
+import { BaseRepository } from './base.repository'
+import { Test } from '@nestjs/testing'
+import { Users } from '../../user/user.entity'
+import { Repository } from 'typeorm';
+import { BaseRepositoryInterface } from './base.repository.interface';
+import { ADP } from '../../helpers/adapters/adapter';
+import { IUser } from '../../user/interfaces';
+
+describe('BaseRepository Test', () => {
+    let baseRepository: BaseRepository<IUser, Users>
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            imports: [],
+	        providers: [
+                BaseRepository,
+                ADP,
+                {
+                    provide: 'BaseRepository',
+                    useClass: Repository
+                },
+            ],
+            controllers: []
+        }).compile()
+
+        baseRepository = module.get<BaseRepository<IUser, Users>>(BaseRepository)
+    })
+
+    it('should create data', async () => {
+        const Mock = jest.fn<Repository<Users>>(() => ({
+            save: jest.fn()
+        }))
+
+        const Adp = jest.fn<ADP<Users>>(() => ({
+            ifToEntity: jest.fn(),
+            ifToEntityCreateCase: jest.fn()
+        }))
+
+        const mock = new Mock()
+
+        const adp = new Adp()
+        const instance = new BaseRepository(mock, adp)
+
+        await instance.create(new Users())
+
+        expect(mock.save).toHaveBeenCalled()
+        expect(adp.ifToEntityCreateCase).toHaveBeenCalled()
+    })
+
+    it('should index all data', async () => {
+        const Mock = jest.fn<Repository<Users>>(() => ({
+            save: jest.fn(),
+            find: jest.fn()
+        }))
+
+        const Adp = jest.fn<ADP<Users>>(() => ({
+            ifToEntity: jest.fn(),
+            ifToEntityCreateCase: jest.fn()
+        }))
+
+        const mock = new Mock()
+
+        const adp = new Adp()
+        const instance = new BaseRepository(mock, adp)
+
+        await instance.findAll(new Users())
+
+        expect(mock.find).toHaveBeenCalled()
+    })
+
+    it('should index all data', async () => {
+        const Mock = jest.fn<Repository<Users>>(() => ({
+            save: jest.fn(),
+            find: jest.fn(),
+            delete: jest.fn()
+        }))
+
+        const Adp = jest.fn<ADP<Users>>(() => ({
+            ifToEntity: jest.fn(),
+            ifToEntityCreateCase: jest.fn()
+        }))
+
+        const mock = new Mock()
+
+        const adp = new Adp()
+        const instance = new BaseRepository(mock, adp)
+
+        await instance.delete(1)
+
+        expect(mock.delete).toHaveBeenCalled()
+    })
+
+    it('has find by id method', async () => {
+        const Mock = jest.fn<Repository<Users>>(() => ({
+            save: jest.fn(),
+            find: jest.fn(),
+            delete: jest.fn(),
+            findOne: jest.fn()
+        }))
+
+        const Adp = jest.fn<ADP<Users>>(() => ({
+            ifToEntity: jest.fn(),
+            ifToEntityCreateCase: jest.fn()
+        }))
+
+        const mock = new Mock()
+
+        const adp = new Adp()
+        const instance = new BaseRepository(mock, adp)
+
+        await instance.findOneById(1)
+
+        expect(mock.findOne).toHaveBeenCalled()
+    })
+})
